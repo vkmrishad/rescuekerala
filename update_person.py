3641607b9cd5351497371a59bf745618d36b4997
Vignesh Hari <vichuhari100@gmail.com>
2018-08-27 23:20:18 +0530

    Resolve Accidental Add

--- update_person.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-
-def hash_person(personobj):
-    person_string = str(personobj.camped_at.id) + personobj.name + personobj.address + str(personobj.phone) + str(personobj.age) + str(personobj.gender) + personobj.notes  
-    result = hashlib.md5(person_string.encode())
-    print(result.hexdigest())
