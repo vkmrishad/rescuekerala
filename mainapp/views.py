28381782791eab84d05fd489e1f46e036b3ba462
Rajeesh Punathil <rajeeshrnair@gmail.com>
2018-08-30 04:33:21 +0530

    Refactored mainapp.views.dmotal view also.
    * Now the code is simple to read.
    * Just one DB hit per call.

--- mainapp/views.py
+++ mainapp/views.py
@@ -19,7 +19,7 @@ from django.contrib.auth import logout
 from django.contrib import admin
 from django.shortcuts import redirect, get_object_or_404
 from django.db.models import Count, QuerySet
-from django.db.models import Case, When
+from django.db.models import Case, When, Sum, F
 from django.core.cache import cache
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
@@ -482,30 +482,27 @@ def dmodist(request):
     return render(request , "dmodist.html" , {"camps" : d }  )
 
 def dmotal(request):
-    if(request.GET.get("district",-1) == -1):return render(request , "dmotal.html"  )
-    dist = request.GET.get("district",-1)
-    if(dist == "all"): data = RescueCamp.objects.filter(status='active').values('taluk').distinct()
-    else:data = RescueCamp.objects.filter(district = dist , status='active').values('taluk').distinct()
-    distmapper = {}
-    for i in districts:
-        distmapper[i[0]] = i[1]
-    d = []
-    for taluk in data :
-        camps = 0 ;total_people = 0 ;total_male = 0 ; total_female = 0 ; total_infant = 0 ; total_medical = 0;district = ""
-        if(dist == "all"):RCdata = RescueCamp.objects.all().filter( taluk = taluk["taluk"] , status="active")
-        else:RCdata = RescueCamp.objects.all().filter( district = dist , taluk = taluk["taluk"] , status="active")
-        for i in RCdata:
-            camps+=1
-            district = i.district
-            total_people += ifnonezero(i.total_people)
-            total_male  += ifnonezero(i.total_males)
-            total_female += ifnonezero(i.total_females)
-            total_infant += ifnonezero(i.total_infants)
-            if(i.medical_req.strip() != ""):total_medical+=1
-
-        d.append( { "district" : distmapper[district] , "taluk" : taluk["taluk"] ,"total_camp" : camps , "total_people" : total_people , "total_male" : total_male , "total_female" : total_female , "total_infant" : total_infant , "total_medical" : total_medical   } )
-    return render(request , "dmotal.html" , {"camps" : d }  )
-
+    dist = request.GET.get("district", -1)
+    if dist == -1:
+        return render(request, "dmotal.html")
+    camp_qs = RescueCamp.objects.filter(status='active')
+    if dist != "all":
+        data = camp_qs.filter(district=dist)
+    distmapper = dict(districts)
+    camps_by_taluk = camp_qs.values('taluk').annotate(
+        total_people=Sum('total_people'), total_male=Sum('total_males'),
+        total_female=Sum('total_females'), total_infant=Sum('total_infants'),
+        total_medical=Count(Case(
+            # Empty strings with or w/o spaces.
+            When(medical_req__regex=r'^[ ]*$', then=1),
+            # Null strings.
+            When(medical_req__isnull=True, then=1))),
+        total_camp=Count('id'), district=Value(distmapper[dist], CharField())
+    ).annotate(
+        # We wanted non-empty, non-null strings but counted the opposite. Reverse now.
+        total_medical=F('total_camp')-F('total_medical')
+    )
+    return render(request, "dmotal.html", {"camps": list(camps_by_taluk)})
 
 def dmocsv(request):
     if("district" not in request.GET.keys()):return HttpResponseRedirect("/")
